b87d45b7f387935d9d5bbe3f2bce1461b21fb991
alexbalak21 <alex.balak@outlook.com>
2023-02-06 22:01:31 +0100

    Inital

---
+++ src/main/java/rest/SpringBootConsumeRestAPI/SpringBootConsumeRestApiApplication.java
@@ -0,0 +1,13 @@
+package rest.SpringBootConsumeRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsumeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConsumeRestApiApplication.class, args);
+	}
+
+}
